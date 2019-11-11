9fd14265a419fada85853b8877c7ae9910f82b60
MohanBabu <57185778+mohanbabu29@users.noreply.github.com>
2019-11-11 07:37:01 +0200

    Add files via upload
    Data 2

---
+++ create_learning2014.R
@@ -0,0 +1,85 @@
+#RS Mohan 6.11.2019
+
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+dim(lrn14)
+#183 obs of 60 variables
+
+str(lrn14)
+# Structure of the data
+
+# Mostly likert scale (1-5) variables
+
+# Includes also Age (positive integers) and gender (as a two level factor: F and M)
+
+# Attitude = Global attitude toward statistics ~Da+Db+Dc+Dd+De+Df+Dg+Dh+Di+Dj
+
+# Points=Exam points (0-33)
+
+summary(lrn14)
+
+library(dplyr)
+# Let's check that Attitude is calculated as defined
+lrn14<-lrn14%>% mutate(Asenne=Da+Db+Dc+Dd+De+Df+Dg+Dh+Di+Dj)
+plot(lrn14$Attitude,lrn14$Asenne)
+
+# Check the distribution of exam points
+hist(lrn14$Points)
+
+# questions related to deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# select the columns related to deep learning, surface learning and strategic learning
+
+deep_columns <- lrn14 %>% select(one_of(deep_questions))
+
+surface_columns <- lrn14 %>% select(one_of(surface_questions))
+
+strategic_columns <- lrn14 %>% select(one_of(strategic_questions))
+# Create column 'attitude' by scaling the column "Attitude"
+
+# Create columns 'deep', 'surf', and 'stra' by averaging
+
+# Exclude observations where the exam point variable is zero
+
+# Select variables gender, age, attitude, deep, stra, surf and points
+
+learning2014 <- lrn14 %>% mutate(
+  
+  attitude=Attitude/10,
+  
+  deep=rowMeans(deep_columns,na.rm=TRUE),
+  
+  surf=rowMeans(surface_columns,na.rm=TRUE),
+  
+  stra=rowMeans(strategic_columns,na.rm=TRUE)
+  
+) %>%
+  
+  filter(Points!=0) %>%
+  
+  select(gender, age=Age, attitude, deep, stra, surf, points=Points)
+
+# Change working directory to IODS-folder
+setwd("~/IODS-project/Data 2")
+
+# Save created data to folder 'data' as an Excel worksheet
+#install.packages("openxlsx")
+library(openxlsx)
+write.xlsx(learning2014,file="~/IODS-project/data 2/learning2014.xlsx")
+
+# Read the data back to R and check that structure and a few first observations look the same
+library(readxl)
+
+readtest <- read.xlsx("~/IODS-project/data 2/learning2014.xlsx")
+readtest2 <- readxl::read_excel("~/IODS-project/data 2/learning2014.xlsx") %>%
+  
+  mutate_at(vars(gender), factor)
+
+str(learning2014)
+str(readtest)
+str(readtest2)
+
+head(learning2014)
+head(readtest)
